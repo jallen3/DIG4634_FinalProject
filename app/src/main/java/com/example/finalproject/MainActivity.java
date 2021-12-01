5dcdf66d6fd8945e33b0c5417576244017e879a7
Jada Allen <47389874+jallen3@users.noreply.github.com>
2021-12-01 17:12:47 -0500

    Added function to gameActivity, test button disabling in MusicHolderActivity, added buttons to MainActivity in order to navigate between activities

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPlayClicked(View view){
+        Intent intent = new Intent(getBaseContext(),GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void onMusicClicked(View view){
+        Intent intent = new Intent(getBaseContext(),MusicHolderActivity.class);
+        startActivity(intent);
     }
 }
 
